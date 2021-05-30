3253c7054c04589d5d8136921dd38e8136963106
Tejas <tejasrgaikwad@gmail.com>
2021-05-30 17:54:26 +0100

    service layer skeleton autowired

--- src/main/java/com/app/brpm/RuleEngineApplication.java
+++ src/main/java/com/app/brpm/RuleEngineApplication.java
@@ -1,10 +1,13 @@
 package com.app.brpm;
 
+import com.app.brpm.service.RulesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RuleEngineApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RuleEngineApplication.class, args);
--- src/main/java/com/app/brpm/controller/RuleEngineController.java
+++ src/main/java/com/app/brpm/controller/RuleEngineController.java
@@ -1,6 +1,9 @@
 package com.app.brpm.controller;
 
+import com.app.brpm.exception.InvalidRequestException;
 import com.app.brpm.model.Order;
+import com.app.brpm.service.RulesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("${server.baseUrl}")
 public class RuleEngineController {
 
+    @Autowired
+    private RulesService rulesService;
+
     @RequestMapping(method = RequestMethod.POST, value = "/rules/payments/outcome", produces = "application/json")
     public ResponseEntity<?> fireRules(@RequestBody Order orderDetails) {
-
-        return new ResponseEntity<>("Invalid Request " , HttpStatus.BAD_REQUEST);
+        try {
+            rulesService.firePaymentsRules(orderDetails);
+            return new ResponseEntity<>(orderDetails, HttpStatus.OK);
+        } catch (InvalidRequestException e) {
+            return new ResponseEntity<>("Invalid Request :" + e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
