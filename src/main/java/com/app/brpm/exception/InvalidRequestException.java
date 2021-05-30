00167958b23c56e6f8944ff3cd4585c0b7daefb0
Tejas <tejasrgaikwad@gmail.com>
2021-05-30 17:08:35 +0100

    Skeleton of spring boot

---
+++ src/main/java/com/app/brpm/exception/InvalidRequestException.java
@@ -0,0 +1,8 @@
+package com.app.brpm.exception;
+
+public class InvalidRequestException extends Exception {
+
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
