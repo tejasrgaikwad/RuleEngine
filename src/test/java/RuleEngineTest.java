5331cdfc8be8207f9064decd4fda71f218a318bf
Tejas <tejasrgaikwad@gmail.com>
2021-05-30 17:35:00 +0100

    Init test cases and setup webapp context

--- src/test/java/RuleEngineTest.java
+++ src/test/java/RuleEngineTest.java
@@ -1,11 +1,27 @@
 import com.app.brpm.RuleEngineApplication;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RuleEngineApplication.class)
 public class RuleEngineTest {
 
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private ObjectMapper objectMapper;
+    private static final String BASE_URL="http://localhost:8080/api/rule-engine/rules/payments/outcome";
+    @Before
+    public void setUp() {
+        this.objectMapper = new ObjectMapper();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
 }
